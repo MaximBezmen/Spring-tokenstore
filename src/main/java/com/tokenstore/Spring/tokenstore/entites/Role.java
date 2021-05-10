b76619cdab50b388ca1a3d85866c5ce64eab53b6
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2021-05-10 17:14:02 +0300

    Add tokenstore

--- src/main/java/com/tokenstore/Spring/tokenstore/entites/Role.java
+++ src/main/java/com/tokenstore/Spring/tokenstore/entites/Role.java
@@ -1,9 +1,12 @@
 package com.tokenstore.Spring.tokenstore.entites;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name="app_role")
+@Data
 public class Role {
     private static final long serialVersionUID = 1L;
     @Id
@@ -16,28 +19,4 @@ public class Role {
     @Column(name="description")
     private String description;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getRoleName() {
-        return roleName;
-    }
-
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
